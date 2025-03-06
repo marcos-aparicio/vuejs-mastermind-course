bbee30729a8b023682d20fcde8fb71045690a479
marcos-aparicio <marcos.aparicio593@gmail.com>
2025-03-06 17:32:36 -0500

    maquetado de la pagina con daisy ui y tailwind

--- todo-list/src/App.vue
+++ todo-list/src/App.vue
@@ -4,44 +4,39 @@
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="Hola mundo" />
+  <nav class="navbar bg-base-200 shadow-sm">
+    <div class="flex-1">
+      <div class="avatar">
+        <div class="w-8">
+          <img src="./assets/logo.svg" alt="logo" />
+        </div>
+      </div>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
+    <span>Todo List App</span>
+  </nav>
+  <main class="flex flex-col my-4 w-swv p-4">
+    <section class="bg-base-200 p-4 rounded w-full">
+      <form action="" class="flex gap-4">
+        <input type="text" placeholder="Todo Title" class="input flex-1" />
+        <div>
+          <button class="btn btn-primary btn-sm">Add Todo</button>
+        </div>
+      </form>
+    </section>
+    <section class="p-4 bg-base-200 m-2">
+      <div class="bg-primary rounded p-2 flex">
+        <p class="flex-1">Pasear al perro</p>
+        <div>
+          <button class="btn btn-xs btn-danger btn-error btn-circle !text-xl">&times;</button>
+        </div>
+      </div>
+    </section>
   </main>
 </template>
+<script>
+  export default {
 
-<style scoped>
-  header {
-    line-height: 1.5;
   }
+</script>
 
-  .logo {
-    display: block;
-    margin: 0 auto 2rem;
-  }
-
-  @media (min-width: 1024px) {
-    header {
-      display: flex;
-      place-items: center;
-      padding-right: calc(var(--section-gap) / 2);
-    }
-
-    .logo {
-      margin: 0 2rem 0 0;
-    }
-
-    header .wrapper {
-      display: flex;
-      place-items: flex-start;
-      flex-wrap: wrap;
-    }
-  }
-</style>
+<style scoped></style>
