07b2fe8a1e009fa327fbcea54ea25939d79b18c8
marcos-aparicio <marcos.aparicio593@gmail.com>
2025-03-06 18:41:42 -0500

    showing how props work, emits and a little bit on how to load tailwindcss

--- todo-list/src/App.vue
+++ todo-list/src/App.vue
@@ -1,8 +1,3 @@
-<script setup>
-  import HelloWorld from './components/HelloWorld.vue'
-  import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
   <nav class="navbar bg-base-200 shadow-sm">
     <div class="flex-1">
@@ -15,6 +10,7 @@
     <span>Todo List App</span>
   </nav>
   <main class="flex flex-col my-4 w-swv p-4">
+    <Alert v-bind:show="showAlert" @close="showAlert = false" message="Please fill the input text" />
     <section class="bg-base-200 p-4 rounded w-full">
       <form action="" class="flex gap-4">
         <input v-model="todoTitle" type="text" placeholder="Todo Title" class="input flex-1" />
@@ -27,7 +23,7 @@
       <div class="bg-primary rounded p-2 flex" v-for="todo in todos" v-bind:key="todo.index">
         <p class="flex-1">{{ todo.title }}</p>
         <div>
-          <button class="btn btn-xs btn-danger btn-error btn-circle !text-xl" @click="()=>removeTodo(todo.index)">
+          <button class="btn btn-xs btn-danger btn-error btn-circle !text-xl" @click="() => removeTodo(todo.index)">
             &times;
           </button>
         </div>
@@ -36,25 +32,36 @@
   </main>
 </template>
 <script>
-  export default {
-    data() {
-      return {
-        lastIndex: 0,
-        todoTitle: "",
-        todos: [],
+
+import Alert from "./components/Alert.vue";
+
+export default {
+  components: { Alert },
+  data() {
+    return {
+      lastIndex: 0,
+      todoTitle: "",
+      showAlert: false,
+      todos: [],
+    }
+  },
+  methods: {
+    addTodo(e) {
+      e.preventDefault();
+
+      if (this.todoTitle === "") {
+        this.showAlert = true;
+        return;
       }
+
+      this.todos.push({
+        title: this.todoTitle,
+        index: this.lastIndex++
+      });
     },
-    methods: {
-      addTodo(e) {
-        e.preventDefault();
-        this.todos.push({
-          title: this.todoTitle,
-          index: this.lastIndex++
-        });
-      },
-      removeTodo(index) {
-        this.todos = this.todos.filter((todo) => todo.index !== index);
-      }
+    removeTodo(index) {
+      this.todos = this.todos.filter((todo) => todo.index !== index);
     }
   }
+}
 </script>
---
+++ todo-list/src/components/Alert.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="alert-error"></div>
+  <div class="alert-info"></div>
+  <div class="alert-success"></div>
+  <div class="alert-warning"></div>
+  <div v-if="show" role="alert"
+    :class="['absolute bottom-0 right-0 alert alert-soft m-2 flex justify-between', `alert-${type}`]">
+    <span class="text-lg">{{ message ?? 'Topuria' }}</span>
+    <button class="btn btn-xs btn-ghost btn-circle !text-xl" @click="closeAlert">
+      &times;
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    message: {
+      required: true,
+      type: String,
+    },
+    show: {
+      required: true,
+      type: Boolean
+    },
+    type: {
+      default: "error",
+      type: String,
+      validator(input) {
+        return ["error", "success", "warning", "info"].includes(input);
+
+      }
+    }
+  },
+  emits: ['close'],
+  methods: {
+    closeAlert() {
+      this.$emit('close');
+    }
+  }
+
+}
+</script>
